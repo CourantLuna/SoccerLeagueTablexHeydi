47fc44d11e7a82f3490896a913b810444bff6bb5
Heydi Garcia Sanchez <courantluna@gmail.com>
2024-02-28 15:34:24 -0400

    Add basic file reading functionality

--- files/src/Main.purs
+++ files/src/Main.purs
@@ -4,7 +4,9 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Console (log)
+import Node.Encoding (Encoding(..))
+import Node.FS.Sync (readTextFile)
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  log =<< readTextFile UTF8 "football.dat"
