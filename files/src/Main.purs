b901a730a300598699bed1404707dea444e85bc6
Heydi Garcia Sanchez <courantluna@gmail.com>
2024-02-28 15:54:48 -0400

    Parse lines from football.dat to extract team data

--- files/src/Main.purs
+++ files/src/Main.purs
@@ -1,12 +1,27 @@
 module Main where
 
 import Prelude
-
+import Data.Maybe (catMaybes)
+import Data.String (split)
+import Data.String.Common (trim)
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Console (logShow)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile)
 
+type FootballData = { team :: String, goalsFor :: Int, goalsAgainst :: Int }
+
+parseFootballData :: String -> Maybe FootballData
+parseFootballData line = case split (== ' ') $ trim line of
+  team : _ : _ : _ : fStr : aStr : _ -> do
+    goalsFor <- readInt fStr
+    goalsAgainst <- readInt aStr
+    Just { team, goalsFor, goalsAgainst }
+  _ -> Nothing
+
 main :: Effect Unit
 main = do
-  log =<< readTextFile UTF8 "football.dat"
+  content <- readTextFile UTF8 "football.dat"
+  let linesOfData = split (== '\n') content
+  let footballData = catMaybes $ map parseFootballData linesOfData
+  traverse_ logShow footballData
